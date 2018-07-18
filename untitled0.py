450acaf56e14b9b5decddbe2afc5dcbe71eebf9f
Qimin <gwenvelaria@gmail.com>
2018-07-18 18:42:36 +0200

    revise

--- untitled0.py
+++ untitled0.py
@@ -11,10 +11,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 
-data1 = sio.loadmat('/Applications/Documents/research/Germany/begin1_1.mat');
-rmse = data1['rmse']
-gl = data1['gl']
-gl_b = data1['gl_b']
+data1 = sio.loadmat('/Users/qimindeng/research/Germany/thres56.mat');
+ge5 = data1['ge5']
+ge6 = data1['ge6']
+gs5 = data1['gs5']
+gs6 = data1['gs6']
+gl5 = data1['gl5']
+gl6 = data1['gl6']
+pgs5 = data1['pgs5']
+pgs6 = data1['pgs6']
 gl_e = data1['gl_e']
 pgl = data1['pgl']
 pgl_b = data1['pgl_b']
@@ -28,7 +33,7 @@ with np.load('/Users/qimindeng/research/Germany/GermanyT.npz') as data:#open the
     lat=data['lat']
     lon=data['lon']
     
-data=np.transpose(rmse)
+data=np.transpose(pgs5)
 lon_0 = (lon[0]+lon[-1])/2
 lat_0 = (lat[0]+lat[-1])/2
 
@@ -54,16 +59,16 @@ ny = data.shape[0]; nx = data.shape[1]
 lons, lats = m.makegrid(nx, ny) # get lat/lons of ny by nx evenly space grid.
 x, y = m(lons, lats) # compute map proj coordinates.
 # draw filled contours.
-#clevs=np.linspace(290,330,41)
-clevs=np.linspace(np.nanmin(data),np.nanmax(data),20)
+clevs=np.linspace(30,100,71)
+#clevs=np.linspace(np.nanmin(data),np.nanmax(data),20)
 #clevs = [6,6.5,7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,12]
-cs = m.contourf(x,y,data,clevs,cmap=cm.s3pcpn_l)
+cs = m.contourf(x,y,data,clevs,cmap='PiYG')
 # add colorbar.
 cbar = m.colorbar(cs,location='bottom',pad="5%")
 #cbar.set_label('days')
 # add title
 plt.title('root mean square error')
-filename = "rmse.png"
+filename = "pgs5.png"
 plt.savefig(filename)
 plt.show()
  
